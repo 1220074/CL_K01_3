f7cf1dd50734e0661abf658bf652c0f88214cdde
RikuEndo <hikituyo.6@gmail.com>
2013-01-09 10:33:42 +0900

    応用プログラミングAの課題1

---
+++ CL_K01_3/CL_K01_3.cpp
@@ -0,0 +1,11 @@
+/* ì¬Ò:1T1(7) ‰““¡—¤ */
+/* ‰Û‘è–¼01_1 */
+
+#include<stdio.h>
+
+void main(void) {
+	double radius;
+	radius = 5.0;
+	printf("”¼Œa%f‚Ì‰~ü‚Í%f‚Å‚·\n", radius, radius*2*3.141593);
+	printf("”¼Œa%f‚Ì‰~‚Ì–ÊÏ‚Í%f‚Å‚·B\n", radius, radius*radius*3.141593);
+}
